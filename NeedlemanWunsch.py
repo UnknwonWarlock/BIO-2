22e0b84247f51d3a2ecccc0eb01cb4c6575a886b
link00000000 <crandall.logan@gmail.com>
2020-10-28 19:39:02 -0400

    Cleanup variable names

--- NeedlemanWunsch.py
+++ NeedlemanWunsch.py
@@ -58,47 +58,48 @@ def genTable(seq1: str, seq2: str, scoring: ScoringCriteria) -> List[List[int]]:
 
             # Initialize to null in the case there is no cell
             # in that direction
-            scoreDiagonal = None
-            scoreUp = None
-            scoreLeft = None
+            score_diagonal = None
+            score_up = None
+            score_left = None
 
             # If we are not in the top row and not in the left-most column
             if(index1 != 0 and index2 != 0):
-                scoreDiagonal = table[index1 - 1][index2 - 1].score
+                score_diagonal = table[index1 - 1][index2 - 1].score
 
                 if(char1 == char2):
-                    scoreDiagonal += scoring.MATCH
+                    score_diagonal += scoring.MATCH
                 else:
-                    scoreDiagonal += scoring.MISMATCH
+                    score_diagonal += scoring.MISMATCH
 
             # If we are not in the top row
             if(index1 != 0):
-                scoreUp = table[index1 - 1][index2].score + scoring.GAP
+                score_up = table[index1 - 1][index2].score + scoring.GAP
 
             # If we are not in the left-most row
             if(index2 != 0):
-                scoreLeft = row[index2 - 1].score + scoring.GAP
+                score_left = row[index2 - 1].score + scoring.GAP
 
             # Find max score of the three options
             # NOTE: This is really gross because `max` cannot handle null values
-            maxScore = max(
-                scoreDiagonal if scoreDiagonal is not None else float('-inf'),
-                scoreUp if scoreUp is not None else float('-inf'),
-                scoreLeft if scoreLeft is not None else float('-inf')
+            max_score = max(
+                score_diagonal if score_diagonal is not None else float(
+                    '-inf'),
+                score_up if score_up is not None else float('-inf'),
+                score_left if score_left is not None else float('-inf')
             )
 
             # Find the direction of the smallest score
             direction = Direction.NONE
 
-            if maxScore == scoreDiagonal:
+            if max_score == score_diagonal:
                 direction = Direction.DIAGONAL
-            elif maxScore == scoreUp:
+            elif max_score == score_up:
                 direction = Direction.UP
-            elif maxScore == scoreLeft:
+            elif max_score == score_left:
                 direction = Direction.LEFT
 
-            row.append(Cell(maxScore, direction))
-            print(Cell(maxScore, direction), end=' | ')
+            row.append(Cell(max_score, direction))
+            print(Cell(max_score, direction), end=' | ')
 
         table.append(row)
         print()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from NeedlemanWunsch import ScoringCriteria, genTable
 
-scoringCriteria = ScoringCriteria(1, -1, -2)
+scoring_criteria = ScoringCriteria(1, -1, -2)
 sequence1 = "AAAC"
 sequence2 = "AGC"
 
 if __name__ == "__main__":
-    table = genTable(sequence1, sequence2, scoringCriteria)
+    table = genTable(sequence1, sequence2, scoring_criteria)
     print('done.')
