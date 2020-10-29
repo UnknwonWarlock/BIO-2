e412789753f65cdecf4c6562e8864516efd6654e
link00000000 <crandall.logan@gmail.com>
2020-10-28 21:13:42 -0400

    Convert RNA to proteins

---
+++ Mutations.py
@@ -0,0 +1,37 @@
+from typing import List, Tuple
+
+proteins_table = {
+    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+    'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
+    'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W',
+}
+
+
+def chunk(input: str, size: int) -> List[str]:
+    return [input[i:i+size] for i in range(0, len(input), size)]
+
+
+def proteins(sequence: str) -> List[str]:
+    # NOTE: Not sure what to do with gaps
+    chunked_sequence = chunk(sequence, 3)
+    proteins = []
+    for codon in chunked_sequence:
+        if codon in proteins_table:
+            proteins.append(proteins_table[codon])
+        else:
+            proteins.append(None)
+
+    return ''.join(proteins)
